849872eede5ef44e93d6a93330bc5dde748184a4
Gery Rosa <rosageri95@gmail.com>
2018-03-04 23:04:09 +0100

    xml parse + book class

---
+++ Bookshelf/Bookshelf/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Bookshelf.Models
+{
+    public class Book
+    {
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+    }
+}
--- Bookshelf/Bookshelf/WebClient/Client.cs
+++ Bookshelf/Bookshelf/WebClient/Client.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Bookshelf.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Bookshelf.WebClient
 {
@@ -12,13 +17,38 @@ namespace Bookshelf.WebClient
         {
             HttpClient client = new HttpClient();
 
-            var uri = new Uri("url");
+            var uri = new Uri("uri");
 
             var response = await client.GetAsync(uri);
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
-                return content;
+                var doc = XDocument.Parse(content);
+
+                var books = new List<Book>();
+
+                foreach(XElement element in doc.Descendants("best_book"))
+                {
+                    var title = element.Element("title").Value;
+                    XElement author = element.Element("author");
+                    var name = author.Element("name").Value;
+                    var book = new Book { Title = title, Author = name };
+
+                    books.Add(book);
+                }
+
+                StringBuilder ret = new StringBuilder();
+                foreach (var book in books)
+                {
+                    ret.Append(book.Author);
+                    ret.Append(", ");
+                    ret.Append(book.Title);
+                    ret.Append("\n");
+
+                }
+                Console.WriteLine(ret.ToString());
+
+                return ret.ToString();
             }
             return "0";
         }
